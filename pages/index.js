895f7ea8c622f1e8c0d5759789d865f64d511c69
Ivica Batinic <ivica.batinic@infinum.hr>
2017-08-15 23:28:33 +0200

    WIP on tuner class and component

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,36 @@
-export default () => (
-  <div>Welcome to next.js!</div>
-)
+import React, { Component } from 'react';
+import Tuner from '../core/tuner';
+import Meter from '../components/meter'
+
+class Index extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      note: {
+        name: 'A',
+        octave: 4,
+        frequency: 440,
+      },
+    }
+  }
+
+  componentDidMount() {
+    this.tuner = new Tuner();
+    this.tuner.onNoteDetected = note => {
+      this.setState({
+        note
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Meter cents={ this.state.note.cents }/>
+        <p>{ this.state.note.frequency.toFixed(1) }</p>
+      </div>
+    );
+  }
+}
+
+export default Index;
