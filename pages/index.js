4f146e81d5f6e0d67a045f779167afdf7f6e7e24
Ivica Batinic <ivica.batinic@infinum.hr>
2017-08-16 00:52:16 +0200

    Reworked tuner to be HOC

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,34 @@
 import React, { Component } from 'react';
-import Tuner from '../core/tuner';
+import tuner from '../core/tuner';
 import { Meter } from '../components'
 
+@tuner
 class Index extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      note: {
-        name: 'A',
-        octave: 4,
-        frequency: 440,
-      },
-    }
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     note: {
+  //       name: 'A',
+  //       octave: 4,
+  //       frequency: 440,
+  //     },
+  //   }
+  // }
 
-  componentDidMount() {
-    this.tuner = new Tuner();
-    this.tuner.onNoteDetected = note => {
-      this.setState({
-        note
-      });
-    }
-  }
+  // componentDidMount() {
+  //   this.tuner = new Tuner();
+  //   this.tuner.onNoteDetected = note => {
+  //     this.setState({
+  //       note
+  //     });
+  //   }
+  // }
 
   render() {
     return (
       <div>
-        <Meter cents={ this.state.note.cents }/>
-        <p>{ this.state.note.frequency.toFixed(1) }</p>
+        <Meter cents={ this.props.note.cents }/>
+        <p>{ this.props.note.frequency.toFixed(1) }</p>
       </div>
     );
   }
