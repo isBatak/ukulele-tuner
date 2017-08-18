c3ba8d8b5cb5b2e3ac674b87b904f3a4abf58ab8
Ivica Batinic <ivica.batinic@infinum.hr>
2017-08-18 16:34:04 +0200

    Add out folder to gitignore

--- out/_next/8649d863-b3bd-4740-a05b-d680b832d55b/page/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/', function() {
-            var comp = module.exports=webpackJsonp([2],[],[369]);
-            return { page: comp.default }
-          })
-        
