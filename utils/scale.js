db796e7e141b9e35b47e1ab3570e1cf35764d8fa
Ivica Batinic <ivica.batinic@infinum.hr>
2017-08-20 16:23:31 +0200

    Release at Sun 20 Aug 2017 16:23:31 CEST

--- utils/scale.js
+++
@@ -1,5 +0,0 @@
-export function scaleRange(input, inputRange, outputRange) {
-  const [inMin, inMax] = inputRange;
-  const [outMin, outMax] = outputRange;
-  return ((input - inMin) / (inMax - inMin) * (outMax - outMin) + outMin);
-}
